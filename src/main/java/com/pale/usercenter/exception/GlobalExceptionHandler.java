eeb092912d5a21ff7d6f99000ebb76f13794550c
PALE13 <palezhou13@163.com>
2023-12-05 17:20:54 +0800

    完成了用户注册，用户注销功能，对返回request的返回对象进行封装，自定义错误码，封装全局异常处理

---
+++ src/main/java/com/pale/usercenter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.pale.usercenter.exception;
+
+import com.pale.usercenter.common.BaseResponse;
+import com.pale.usercenter.common.ErrorCode;
+import com.pale.usercenter.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author pale
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+    }
+}
---
+++ src/main/java/com/pale/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.pale.usercenter.mapper;
+
+import com.pale.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
